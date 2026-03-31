1037a5ac5c2d17fe4fd0be3f00f83fe781f1be8e
Bibek Ray <bibek.ray@deerwalk.edu.np>
2026-03-31 22:51:46 +0545

    feat: update Home component to fetch and display users, remove unused SVG files

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/prisma";
 import { cn } from "@/lib/utils";
 import { CheckCircle2Icon } from "lucide-react";
 
-export default function Home() {
+export default async function Home() {
   const name = false;
+
+  const users = await prisma.user.findMany();
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -12,19 +14,22 @@ export default function Home() {
         <h1
           className={cn(
             "text-3xl font-bold mb-4 font-mono",
-            name ? "text-purple-700" : "text-purple-700",
+            name ? "text-grey-700" : "text-grey-700",
           )}
         >
           Welcome Ray!
         </h1>
-        <Alert className="bg-purple-600/10 border-purple-600/50 text-purple-600">
+        <Alert className="bg-grey-600/10 border-grey-600/50 text-grey-600">
           <CheckCircle2Icon />
           <AlertTitle>Payment successful</AlertTitle>
-          <AlertDescription className="text-sm text-purple-800/90">
+          <AlertDescription className="text-sm text-grey-800/90">
             Your payment of $29.99 has been processed. A receipt has been sent
             to your email address.
           </AlertDescription>
         </Alert>
+        <ul>
+          {JSON.stringify(users)}
+        </ul>
       </section>
     </div>
   );
